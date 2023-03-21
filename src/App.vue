52fc6a6cac838ebb2f226ef98e6cc71b81f68dd0
Mayu Imanaka <imamayu.1215@gmail.com>
2023-03-21 17:53:06 +0900

    practice v-for

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,71 @@
-<script setup>
+<script>
+export default {
+  data(){
+    return {
+      title:'My New Vue Title',
+      message: 'Welcome to Vue',
+      titleStatus: {
+        red: true,
+        fontBig: true,
+      },
+      input: {
+        firstName: '',
+        lastName: '',
+        isMember: true,
+      },
+      users: [
+      {
+        firstName: 'John',
+        lastName: 'Smith',
+        isMember: false,
+      },
+      {
+        firstName: 'Taro',
+        lastName: 'Shinjuku',
+        isMember: false,
+      },
+      {
+        firstName: 'Hanako',
+        lastName: 'Kouenji',
+        isMember: true,
+      }
+      ],
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.input)
+      this.input = {
+        firstName: '',
+        lastName: '',
+        isMember: true,
+      }
+    },
+  },
+}
 </script>
 
 <template>
-<h1>My Vue Playground</h1>
+<h1 :title="message" :class="titleStatus">{{ title }}</h1>
+<input type="text" v-model="input.firstName" />
+<input type="text" v-model="input.lastName" />
+<input type="checkbox" v-model="input.isMember"/>
+
+<button @click="addUser">ADD User!</button>
+<h2>ユーザーデータ</h2>
+<div v-for="user in users">
+  <p>Name: {{ user.firstName + ' ' + user.lastName }}</p>
+  <p v-if="user.isMember">メンバーです</p>
+  <p v-else>メンバーではありません</p>
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+
+<style>
+.red {
+  color: red;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.fontBig{
+  font-size: 20px;
 }
 </style>
